39938153ca51dbd44431594edec55d8eb6fe8430
Sydney Hull <sydneyjhull@gmail.com>
2023-02-06 09:48:05 -0700

    woot

--- Mission4Assignment(1)/wwwroot/lib/js/GradeCalculator.js
+++ Mission4Assignment(1)/wwwroot/lib/js/GradeCalculator.js
@@ -1,46 +1,66 @@
 ﻿$("#btnSubmit").click(function () {
+    // assign inputs to variables
     let assignments = $("#assignments").val();
     let projects = $("#projects").val();
     let quizzes = $("#quizzes").val();
     let midterm = $("#midterm").val();
     let final = $("#final").val();
     let intex = $("#intex").val();
-    let grade = assignments + projects + quizzes + midterm + final + intex;
+    let letterGrade = "";
+    var result = $("#result");
 
+
+    //change for weight
+    let percentAssignments = .5 * parseFloat(assignments);
+    let percentProjects = .1 * parseFloat(projects);
+    let percentQuizzes = .1 * parseFloat(quizzes);
+    let percentMidterm = .1 * parseFloat(midterm);
+    let percentFinal = .1 * parseFloat(final);
+    let percentIntex = .1 * parseFloat(intex);
+
+
+    //final grade 
+    let grade = percentAssignments + percentProjects + percentQuizzes + percentMidterm + percentFinal + percentIntex;
+
+
+    //print results
     if (grade >= 94) {
-        alert("You have an A in the class")
+        letterGrade = "A";
     }
-    else if (grade >= 90) {
-        print("You have an A- in the class")
+    else if (grade < 94 && grade >= 90) {
+        letterGrade = "A-";
     }
-    else if (grade >= 87) {
-        print("You have a B+ in the class")
+    else if (grade < 90 && grade >= 87) {
+        letterGrade = "B+";
     }
-    else if (grade >= 84) {
-        print("You have a B in the class")
+    else if (grade < 87 && grade >= 84) {
+        letterGrade = "B";
     }
-    else if (grade >= 80) {
-        print("You have a B- in the class")
+    else if (grade < 84 && grade >= 80) {
+        letterGrade = "B-";
     }
-    else if (grade >= 77) {
-        print("You have a C+ in the class")
+    else if (grade < 80 && grade >= 77) {
+        letterGrade = "C+";
     }
-    else if (grade >= 74) {
-        print("You have a C in the class")
+    else if (grade < 77 && grade >= 74) {
+        letterGrade = "C";
     }
-    else if (grade >= 70) {
-        print("You have a C- in the class")
+    else if (grade < 74 && grade >= 70) {
+        letterGrade = "C-";
     }
-    else if (grade >= 67) {
-        print("You have a D+ in the class")
+    else if (grade < 70 && grade >= 67) {
+        letterGrade = "D+";
     }
-    else if (grade >= 64) {
-        print("You have a D in the class")
+    else if (grade < 67 && grade >= 64) {
+        letterGrade = "D";
     }
-    else if (grade >= 60) {
-        print("You have a D- in the class")
+    else if (grade < 64 && grade >= 60) {
+        letterGrade = "D-";
     }
     else {
-        print("You have an E in the class")
+        letterGrade = "E";
     }
+
+    let finalString = "You got a(n) " + letterGrade + " in the class."
+    $("#result").html(finalString)
 })  
